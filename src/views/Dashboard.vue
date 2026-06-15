0f542bf31eca5731dea4bff0a09a4bdc91744ba5
rizkyyurdan26 <rizkyyurdan26@gmail.com>
2026-06-15 14:54:24 +0700

    setup style

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
-        <h1 class="text-danger text-2xl">Testing</h1>
+    <div class="h-screen w-full bg-gradient-to-b from-blue-600 to-blue-700">
+        <h1 class="text-primary1 text-2xl">Testing</h1>
+        <h1 class="text-primary2 text-2xl">Testing</h1>
     </div>
 </template>
